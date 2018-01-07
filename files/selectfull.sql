3cc56c173656b880bd07544d718110f895e2d70f
Léo Costa <leoeduar@gmail.com>
2018-01-07 03:25:09 -0200

    Update selectfull.sql

--- files/selectfull.sql
+++ files/selectfull.sql
@@ -4,9 +4,9 @@
 		pf.status					status, 
 		pre.valor					valorItem, 
 		p.id 						pedido, 
-		p.datapedido 				pedidoData,
-		pre.quantidademinima 		qtdeMinima, 
-		pi.quantidadesolicitada 	qtdeSolicitada
+		p.datapedido 					pedidoData,
+		pre.quantidademinima 				qtdeMinima, 
+		pi.quantidadesolicitada 			qtdeSolicitada
 	from pedido p
 	inner join pedidoitem pi
 			on p.id = pi.pedido
